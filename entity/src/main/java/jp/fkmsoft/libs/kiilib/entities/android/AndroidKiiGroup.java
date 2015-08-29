c4cef929b5db7c59c7263a3c3ff25f779e23b829
hrk fkm <fkm@fkmsoft.jp>
2015-08-29 18:18:40 +0900

    Add getName()

--- entity/src/main/java/jp/fkmsoft/libs/kiilib/entities/android/AndroidKiiGroup.java
+++ entity/src/main/java/jp/fkmsoft/libs/kiilib/entities/android/AndroidKiiGroup.java
@@ -3,6 +3,9 @@ package jp.fkmsoft.libs.kiilib.entities.android;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jp.fkmsoft.libs.kiilib.entities.BucketOwnable;
 import jp.fkmsoft.libs.kiilib.entities.KiiGroup;
 
@@ -13,11 +16,13 @@ public class AndroidKiiGroup implements KiiGroup, Parcelable {
     private final String mId;
     private String mName;
     private AndroidKiiUser mOwner;
+    private List<AndroidKiiUser> mMembers;
 
     public AndroidKiiGroup(String id, String name, AndroidKiiUser owner) {
         mId = id;
         mName = name;
         mOwner = owner;
+        mMembers = new ArrayList<>();
     }
 
     AndroidKiiGroup(Parcel source) {
@@ -26,6 +31,11 @@ public class AndroidKiiGroup implements KiiGroup, Parcelable {
         int flag = source.readInt();
         if (flag == 1) {
             mOwner = new AndroidKiiUser(source);
+        }
+        int count = source.readInt();
+        mMembers = new ArrayList<>(count);
+        for (int i = 0 ; i < count ; ++i) {
+            mMembers.add(new AndroidKiiUser(source));
         }
     }
 
@@ -85,6 +95,10 @@ public class AndroidKiiGroup implements KiiGroup, Parcelable {
         } else {
             dest.writeInt(1);
             mOwner.writeToParcel(dest, flags);
+        }
+        dest.writeInt(mMembers.size());
+        for (AndroidKiiUser member : mMembers) {
+            member.writeToParcel(dest, flags);
         }
     }
 }
